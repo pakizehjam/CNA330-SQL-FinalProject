16fbbd9c52becfe9473982aee39529ee08cc9b47
luma24 <57962904+luma24@users.noreply.github.com>
2020-12-09 17:54:29 -0800

    Update Plotting.py

--- Plotting.py
+++ Plotting.py
@@ -18,7 +18,7 @@ connection = engine.connect()
 # Ploting
 # Luma's part
 # Plots the quantity of Apps that have Free Type with their Rating level.
-# ( We can see here that this Database have 3910 FREE Apps and have a rate between 4.2 and 4.6)
+# ( We can see here that this Database have 3667 FREE Apps and have a rate between 4.2 and 4.6)
 plt.title("Free Apps")
 df[df["Type"] == "Free"]["Rating"].plot(kind='hist')
 plt.ylabel("Quantity")
@@ -28,7 +28,7 @@ plt.show()
 
 # Hasan's part
 # Plots the quantity of Apps that have Paid Type with their Rating level.
-# ( We can see here that this Database have 263 PAID Apps and have a highest rate between 4.2 and 4.6)
+# ( We can see here that this Database have 245 PAID Apps and have a highest rate between 4.2 and 4.6)
 
 plt.title("Paid Apps")
 df[df["Type"] == "Paid"]["Rating"].plot(kind="hist")
@@ -44,7 +44,7 @@ plt.show()
 
 # Luma's part
 # Find the number of Rate for a specific category(BUSINESS)
-# (We can see that the database has 115 apps with a highest rate for business category is between 4.2nd 4.6)
+# (We can see that the database has 96 apps with a highest rate for business category is between 4.2nd 4.6)
 plt.title("Number of apps under the BUSINESS Category")
 df[df["Category"] == "BUSINESS"]['Rating'].plot(kind="hist")
 plt.ylabel("Quantity")
